2980336dcee8808804ce8baed55fd0f841f30ca1
dgknzts <oztasdogukan@gmail.com>
2025-12-14 13:20:00 +0300

    applications

---
+++ applications.m
@@ -0,0 +1,27 @@
+%% Application of L'Hopital's:  Racing functions to infinitiy
+% Which function grows faster to inf:
+% to determine we can ratio them as
+% lim x->inf f(x) / g(x) .
+
+% Possible answers:
+% inf = Numerator faster
+% 0 = Denominator faster
+% r = growth ratio settles to a value
+% !!! Use L-Hopital's again and again to solve them!
+
+
+% Note: The natural exponent grows to infinity faster than
+% any power (or any non-infinitely differentiable function)
+
+
+%% Second Derivative Test
+% Purpose: distinguish minimum and maximum as critical points
+
+% if ~x is a critical point of f, and
+% if f'(~x) and f''(~x) exist, then:
+
+% f''(~x) > 0 -> f(~x) is a local min
+% f''(~x) < 0 -> f(~x) is a local max
+% f''(~x) = 0 -> inconclusive
+
+ 
